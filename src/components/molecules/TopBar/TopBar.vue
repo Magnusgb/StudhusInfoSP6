bebf9e9f2497f10cee32b054c901e02b9db6ac55
alogmedia <albertlogn@gmail.com>
2024-05-06 14:00:03 +0200

    Atomic Design updates

---
+++ src/components/molecules/TopBar/TopBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AtomLogo from '@/components/atoms/Logo/Logo/AtomLogo.vue';
+import Flag from '@/components/atoms/Logo/Flags/Flag.vue';
+</script>
+
+<template>
+<header>
+  <AtomLogo />
+    <div class="flags">
+      <Flag src="/src/assets/images/icons/dk.svg" alt="Danish Flag" />
+      <Flag src="/src/assets/images/icons/en.svg" alt="English Flag" />
+    </div>
+</header>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
+import LandingPage from '@/views/LandingPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'LandingPage',
+      component: LandingPage
     },
     /*{
       path: '/about',
