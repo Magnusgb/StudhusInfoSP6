81131ee7702d5c9828c91dfa85b62fae4fe42976
Magnus Bomholt <hello@magnusgb.dk>
2024-05-08 11:47:33 +0200

    GradientSectionBack

---
+++ src/components/molecules/BackToFP.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="backtofp-section ">
+            <AtomSquareButton />
+            <AtomH3Title>Hej</AtomH3Title>
+            <AtomParagraph>Med dig</AtomParagraph>
+    </div>
+</template>
+
+<script setup>
+import AtomH3Title from '@/components/atoms/AtomH3Title.vue';
+import AtomParagraph from '@/components/atoms/AtomParagraph.vue';
+import AtomSquareButton from '@/components/atoms/Buttons/SquareButton/AtomSquareButton.vue';
+</script>
+
+<style scoped>
+    .backtofp-section {
+    background: rgb(220,81,188);
+    background: linear-gradient(152deg, rgba(220,81,188,1) 20%, rgba(254,145,87,1) 40%, rgba(200,93,164,1) 60%, rgba(108,103,228,1) 80%, rgba(200,93,164,1) 100%);
+}
+</style>
+  
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -3,13 +3,12 @@ import AtomBackground from '@/components/atoms/AtomBackground.vue';
 import TopBar from '@/components/organisms/TopBar/TopBar.vue';
 import HeroBanner from '@/components/organisms/HeroBanner.vue';
 import BookRoomSection from '@/components/organisms/BookRoomSection.vue';
+import AtomGradientBG from '@/components/atoms/AtomGradientBG.vue';
+import BackToFP from '@/components/molecules/BackToFP.vue';
 </script>
 
 <template>
   <main>
-    <AtomBackground />
-    <TopBar />
-    <HeroBanner />
-    <BookRoomSection />
+   <BackToFP />
   </main>
 </template>
