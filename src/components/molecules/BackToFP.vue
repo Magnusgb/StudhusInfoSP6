f2883f9dacc23e0a088bc1bbe458c460f2a47574
alogmedia <albertlogn@gmail.com>
2024-05-08 13:06:00 +0200

    Page fix

--- src/components/molecules/BackToFP.vue
+++ src/components/molecules/BackToFP.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="backtofp-section ">
-            <AtomSquareButtonLeft buttonTitle='TILBAGE'/>
+            <AtomSquareButtonLeft buttonTitle='TILBAGE' @click="navigateToFront"/>
             <div class="backtofp-text">
             <AtomH3Title>KOMMENDE EVENTS</AtomH3Title>
             <AtomParagraph>Hos Studenterhuset Odense</AtomParagraph>
@@ -12,6 +12,20 @@
 import AtomH3Title from '@/components/atoms/AtomH3Title.vue';
 import AtomParagraph from '@/components/atoms/AtomParagraph.vue';
 import AtomSquareButtonLeft from '@/components/atoms/Buttons/SquareButton/AtomSquareButtonLeft.vue';
+
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigateToFront = () => {
+  // Use the router instance to navigate to the "/events" route
+  router.push('/');
+};
+
+// Expose the navigateToEvents function to the template
+defineExpose({
+    navigateToFront
+});
 </script>
 
 <style scoped>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,10 +1,9 @@
 <script setup>
 import FrontPage from '@/components/pages/FrontPage.vue';
-import BackToFP from '@/components/molecules/BackToFP.vue';
 </script>
 
 <template>
   <main>
-   <BackToFP />
+   <FrontPage />
   </main>
 </template>
